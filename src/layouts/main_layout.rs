12a530f3b5368e98585b97aca55b9986fa143a01
Aleksandr Tretyakov <flonednb@gmail.com>
2022-09-22 20:21:46 +0500

    Replace engine path with GFxExport path.

--- src/layouts/main_layout.rs
+++ src/layouts/main_layout.rs
@@ -1,19 +1,20 @@
 // External.
 use druid::widget::{prelude::*, Button};
 use druid::widget::{Flex, Label, MainAxisAlignment, Padding};
-use native_dialog::{FileDialog, MessageDialog, MessageType};
+use native_dialog::FileDialog;
 
 // Custom.
 use crate::ApplicationState;
 
 // Layout customization.
 const TEXT_SIZE: f64 = 18.0;
+const SMALL_TEXT_SIZE: f64 = 14.0;
 const LEFT_COLUMN_SIZE: f64 = 0.4;
 const RIGHT_COLUMN_SIZE: f64 = 0.6;
 
 #[derive(Clone, Data)]
 pub struct MainLayout {
-    pub path_to_engine: String,
+    pub path_to_gfxexport_bin: String,
     pub path_to_swf_file: String,
     pub path_to_gfx_dir: String,
     pub path_to_xml_dir: String,
@@ -30,25 +31,28 @@ impl MainLayout {
             Flex::column()
                 .main_axis_alignment(MainAxisAlignment::Start)
                 .must_fill_main_axis(true)
+                .with_default_spacer()
                 .with_flex_child(
                     Flex::row()
                         .with_flex_child(
                             Button::from_label(
-                                Label::new("Path to engine:").with_text_size(TEXT_SIZE),
+                                Label::new("Path to GFxExport:").with_text_size(TEXT_SIZE),
                             )
-                            .on_click(MainLayout::on_select_engine_clicked),
+                            .on_click(MainLayout::on_select_gfxexport_bin_clicked),
                             LEFT_COLUMN_SIZE,
                         )
                         .with_default_spacer()
                         .with_flex_child(
                             Label::new(|data: &ApplicationState, _env: &_| {
-                                data.main_layout.path_to_engine.clone()
+                                data.main_layout.path_to_gfxexport_bin.clone()
                             })
-                            .with_text_size(TEXT_SIZE),
+                            .with_text_size(SMALL_TEXT_SIZE),
                             RIGHT_COLUMN_SIZE,
                         ),
                     1.0,
                 )
+                .with_default_spacer()
+                .with_default_spacer()
                 .with_default_spacer()
                 .with_flex_child(
                     Flex::row()
@@ -64,7 +68,7 @@ impl MainLayout {
                             Label::new(|data: &ApplicationState, _env: &_| {
                                 data.main_layout.path_to_swf_file.clone()
                             })
-                            .with_text_size(TEXT_SIZE),
+                            .with_text_size(SMALL_TEXT_SIZE),
                             RIGHT_COLUMN_SIZE,
                         ),
                     1.0,
@@ -85,12 +89,31 @@ impl MainLayout {
                             Label::new(|data: &ApplicationState, _env: &_| {
                                 data.main_layout.path_to_gfx_dir.clone()
                             })
-                            .with_text_size(TEXT_SIZE),
+                            .with_text_size(SMALL_TEXT_SIZE),
                             RIGHT_COLUMN_SIZE,
                         ),
                     1.0,
                 ),
         )
+    }
+
+    fn on_select_gfxexport_bin_clicked(
+        _ctx: &mut EventCtx,
+        data: &mut ApplicationState,
+        _env: &Env,
+    ) {
+        // Get path to GFxExport1.exe file.
+        let path = FileDialog::new()
+            .add_filter("GFxExport file", &["exe"])
+            .show_open_single_file()
+            .unwrap();
+        if path.is_none() {
+            return;
+        }
+        let path = path.unwrap();
+
+        // Save.
+        data.main_layout.path_to_gfxexport_bin = path.to_string_lossy().to_string();
     }
 
     fn on_select_swf_clicked(_ctx: &mut EventCtx, data: &mut ApplicationState, _env: &Env) {
@@ -133,44 +156,13 @@ impl MainLayout {
         // Save.
         data.main_layout.path_to_gfx_dir = path.to_string_lossy().to_string();
     }
-
-    fn on_select_engine_clicked(_ctx: &mut EventCtx, data: &mut ApplicationState, _env: &Env) {
-        // Get path to engine directory.
-        let path = FileDialog::new().show_open_single_dir().unwrap();
-        if path.is_none() {
-            return;
-        }
-        let path = path.unwrap();
-
-        // Check that this directory contains "Tools/GFxExport" directories.
-        let mut path_to_tool = path.clone();
-        path_to_tool.push("Tools");
-        path_to_tool.push("GFxExport");
-
-        if !path_to_tool.exists() {
-            MessageDialog::new()
-                .set_type(MessageType::Error)
-                .set_title("Error")
-                .set_text(
-                    "Path to engine should point to engine directory where file \
-                    \"cryengine.cryengine\" is located.",
-                )
-                .show_alert()
-                .unwrap();
-            Self::on_select_engine_clicked(_ctx, data, _env);
-            return;
-        }
-
-        // Save.
-        data.main_layout.path_to_engine = path.to_string_lossy().to_string();
-    }
 }
 
 impl Default for MainLayout {
     fn default() -> Self {
         Self {
             path_to_swf_file: String::new(),
-            path_to_engine: String::new(),
+            path_to_gfxexport_bin: String::new(),
             path_to_gfx_dir: String::new(),
             path_to_xml_dir: String::new(),
         }
