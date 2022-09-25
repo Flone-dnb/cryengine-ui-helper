5a1b47e0209e2bf86836da70ff8777ee59d68bca
Aleksandr Tretyakov <flonednb@gmail.com>
2022-09-25 14:55:36 +0500

    Theme improvements.

--- src/misc/theme.rs
+++ src/misc/theme.rs
@@ -42,21 +42,21 @@ impl Theme {
         match self {
             Self::DarkOrange => ColorPalette {
                 base: BaseColors {
-                    background: Color::from_rgb8(20, 20, 20),
+                    background: Color::from_rgb8(30, 30, 30),
                     foreground: Color::from_rgb8(45, 45, 45),
                 },
                 normal: NormalColors {
                     primary: Color::from_rgb8(147, 89, 26),
                     secondary: Color::from_rgb8(152, 51, 247),
                     alt: Color::from_rgb8(21, 225, 157),
-                    dangerous: Color::from_rgb8(199, 53, 24),
+                    dangerous: Color::from_rgb8(119, 53, 24),
                     surface: Color::from_rgb8(130, 130, 130),
                 },
                 bright: BrightColors {
                     primary: Color::from_rgb8(244, 155, 53),
                     secondary: Color::from_rgb8(170, 121, 216),
                     alt: Color::from_rgb8(41, 245, 177),
-                    dangerous: Color::from_rgb8(219, 73, 44),
+                    dangerous: Color::from_rgb8(229, 73, 44),
                     surface: Color::from_rgb8(224, 224, 224),
                 },
             },
